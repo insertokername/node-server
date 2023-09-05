7ca98a991d9f12a2eb8473ec9c91514c9e1fac3a
insertokname <insertokname@gmail.com>
2023-09-05 23:26:09 +0300

    added task updater

---
+++ update_task.bat
@@ -0,0 +1,16 @@
+rem @echo off
+
+set TaskName=PYTHON_STREAM-take-ss
+set BatchDir=%~dp0
+set BatFile=hidden_local_ss.vbs
+
+rem Check if the task exists and delete it if it does
+schtasks /query /TN "%TaskName%" 2>nul
+if %errorlevel% equ 0 (
+    schtasks /delete /TN "%TaskName%" /F
+)
+
+rem Create a new task
+schtasks /create /TN "%TaskName%" /TR "%BatchDir%%BatFile%" /SC ONCE /SD 10/10/2010 /ST 00:00
+
+pause
