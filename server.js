e6aa7fe41140a7211b13c1f1925dd13d03c234f9
insertokname <insertokname@gmail.com>
2023-09-05 22:58:34 +0300

    forgor to add node modules 😊

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const { json } = require("express");
+const app = express();
+const port = 3000
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
 const WebSocket = require("ws");
 const server = new WebSocket.Server({ port: 8080 });
+
 
 const connectedSockets = new Map(); //socket:socket, user/client
 
